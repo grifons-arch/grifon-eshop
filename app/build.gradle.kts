c464e0daefae83169ee2e019c80db8d918507996
grifons-arch <grifons@gmail.com>
2026-01-25 18:26:51 +0200

    Add explicit material icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material.icons.extended)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.material)
