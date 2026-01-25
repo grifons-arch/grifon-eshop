fe48d70aed629579478a4ce274ab80c4202c72de
grifons-arch <grifons@gmail.com>
2026-01-25 16:31:56 +0200

    Bump app version and update readme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.grifon.eshop"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     flavorDimensions += "shop"
