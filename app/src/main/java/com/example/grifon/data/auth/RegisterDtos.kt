a68e29fa79a0ce71c5508e6bc88015694ba6f441
grifons-arch <grifons@gmail.com>
2026-01-28 11:51:56 +0200

    Fix register password field

--- app/src/main/java/com/example/grifon/data/auth/RegisterDtos.kt
+++ app/src/main/java/com/example/grifon/data/auth/RegisterDtos.kt
@@ -1,10 +1,7 @@
 package com.example.grifon.data.auth
 
-import com.squareup.moshi.Json
-
 data class RegisterRequestDto(
     val email: String,
-    @Json(name = "passwd")
     val password: String,
     val socialTitle: String? = null,
     val firstName: String,
